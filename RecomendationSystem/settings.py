3d13357da6d1d6068ce6e1ff8eed41d4aa376431
MartinAAcebeyL <martinaacbyl2000@gmail.com>
2022-11-06 11:46:12 -0400

    models

--- RecomendationSystem/settings.py
+++ RecomendationSystem/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'RecomendationSystem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('NAME'),
+        'USER': 'root',
+        'PASSWORD':config('PASSWORD'),
+        'HOST': config('HOST'),
+        'PORT': config('PORT'),
     }
 }
 
--- apps/usuarios/models.py
+++ apps/usuarios/models.py
@@ -1,3 +1,60 @@
 from django.db import models
-
+from django.contrib.auth.models import BaseUserManager, AbstractUser
+from django.contrib.auth.hashers import make_password, check_password
 # Create your models here.
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password, **extra_fields):
+        """
+        Create and save a User with the given email and password.
+        """
+        if not email:
+            raise ValueError('The Email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        """
+        Create and save a SuperUser with the given email and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        return self.create_user(email, password, **extra_fields)
+
+
+class Usuario(AbstractUser):
+    name = models.CharField(max_length=255, null=True, blank=True)
+    last_name = models.CharField(max_length=255, null=True, blank=True)
+    username = models.CharField(max_length=255, null=True, blank=True)
+    email = models.EmailField(max_length=255, unique=True, null=False)
+    password = models.CharField(max_length=100, null=False)
+    remember_password = models.CharField(max_length=100, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name', 'last_name']
+    
+    def __str__(self):
+        return f'Usuario/ {self.name} {self.last_name}'
+
+    def set_password(self, password):
+        self.password = make_password(password)
+        self.save()
+
+    def check_password(self, password):
+        """Returns true if this is the user's password, false
+        otherwise """
+        return check_password(password, self.password)
